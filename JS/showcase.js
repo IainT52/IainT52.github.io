8cf6654d917a1b645ab45aa7a822fc1393baa2a4
IainT52 <iaintier@buffalo.edu>
2020-09-23 15:07:22 -0400

    small fixes and update to card scroll animation

--- JS/showcase.js
+++ JS/showcase.js
@@ -1,11 +1,12 @@
-// // Card showcase scrolling animation by moving your mouse over the cards
+// Card showcase scrolling animation by moving your mouse over the cards
 $(document).ready(function() {
     setTimeout(function() {
         let section = $('section');
         section.mousemove(function(event) {
             var mouseX = event.pageX,
-            offset = $('body').width() * .15;
-            section.scrollLeft(5000 / $('body').width() * ((mouseX - offset)/2));
+            offset = $('body').width() * .25;
+            // animate here to make scrolling smoother -- 15ms animation is ideal to prevent stuttering
+            section.animate({scrollLeft: (5000 / $('body').width() * ((mouseX - offset)/2))},15);
         });
     }, 2000);
 });
