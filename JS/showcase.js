34fac95a92029cecb013ce457f5a4ba4b50226b9
IainT52 <iaintier@buffalo.edu>
2020-09-27 13:11:10 -0400

    Added resume link and add pre loader to projects page

--- JS/showcase.js
+++ JS/showcase.js
@@ -1,3 +1,10 @@
+// Pre loader delay animation
+$(function(){
+    $('.preloader svg').delay(2000).fadeOut('slow');
+    $('.preloader p').delay(2500).fadeOut(1000);
+    $('.preloader').delay(2300).fadeOut(1000);
+})
+
 // Card showcase scrolling animation by moving your mouse over the cards
 $(document).ready(function() {
     setTimeout(function() {
